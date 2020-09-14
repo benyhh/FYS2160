5c3d68d4799e4b2a9a109bffc6abfa43cdbadbfb
Bendik <bendik98ny@gmail.com>
2020-09-14 14:36:42 +0200

    First commit

---
+++ Oblig/oblig1/1.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import numpy as  np
+
+f = open('termokopper.txt', 'r')
+lines = f.readlines()
+
+time = np.zeros(len(lines))
+temp1 = np.zeros(len(lines))
+temp2 = np.zeros(len(lines))
+
+for i in range(len(lines)):
+    split = lines[i].split()
+    time[i] = split[0]
+    temp1[i] = split[1]
+    temp2[i] = split[2]
+
+plt.plot(time,temp1,time,temp2)
+plt.legend(["Temperfect", "Bodum"])
+plt.title("Temperature of liquid in cups")
+plt.xlabel("Time [s]")
+plt.ylabel("Temperature [°C]")
+#plt.savefig("cuptemp.png", dpi = 400)
+plt.show()
